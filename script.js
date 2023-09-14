741005386a01397ebaf6111bd2b2c2cd9ff469cb
Tahseenaasif <94477366+Tahseenaasif@users.noreply.github.com>
2023-09-15 03:33:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ let hour=document.getElementById("hour");
+ let minute=document.getElementById("minute");
+ let second=document.getElementById("second");
+setInterval(()=>{
+    d=new Date();
+    htime=d.getHours();
+    mtime=d.getMinutes();
+    stime=d.getSeconds();
+    hrotations=30*htime+mtime/2
+    mrotations=6*mtime;
+    srotations=6*stime;
+    hour.style.transform=`rotate(${hrotations}deg)`;
+    minute.style.transform=`rotate(${mrotations}deg)`;
+    second.style.transform=`rotate(${srotations}deg)`;
+
+},1000)
